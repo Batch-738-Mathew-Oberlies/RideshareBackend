ecdd5f7da0b454060f3d09fdc9e9d7eed076eb30
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-22 15:26:21 -0400

    Revert "Merge branch 'documentation' of https://github.com/Batch-738-Mathew-Oberlies/RideshareBackend into LoggerSetup"
    This reverts commit 96d71c417a5e5b7670719ddc5280beaddc7db76a, reversing
    changes made to cfff272eae45c8acb58451311f02e893fadb948b.

--- src/main/java/com/revature/models/Batch.java
+++ src/main/java/com/revature/models/Batch.java
@@ -2,7 +2,10 @@ package com.revature.models;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 
 import org.springframework.stereotype.Component;
--- src/main/java/com/revature/models/Car.java
+++ src/main/java/com/revature/models/Car.java
@@ -2,7 +2,14 @@ package com.revature.models;
 
 import java.io.Serializable;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Positive;
 
