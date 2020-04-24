4fb012f62d45922caaae46391a730a4930770603
Philip Lawrence <philip.lawrence.1992@gmail.com>
2020-04-24 16:05:13 -0400

    Added ability for riders to join and be removed from trips. Added TripDTO

--- src/main/java/com/revature/controllers/TripController.java
+++ src/main/java/com/revature/controllers/TripController.java
@@ -1,17 +1,25 @@
 package com.revature.controllers;
 
-import com.revature.models.Trip;
-import com.revature.services.TripService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
-import java.util.List;
-import java.util.Optional;
+import com.revature.models.Trip;
+import com.revature.models.TripDTO;
+import com.revature.models.User;
+import com.revature.models.UserDTO;
+import com.revature.services.TripService;
+import com.revature.services.UserService;
+
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 
 @RestController
 @RequestMapping("/trips")
@@ -20,6 +28,8 @@ import java.util.Optional;
 public class TripController {
     @Autowired
     private TripService tripService;
+    @Autowired
+    private UserService userService;
 
     /**
      * HTTP GET method (/trips)
@@ -28,10 +38,14 @@ public class TripController {
      */
     @ApiOperation(value = "Return all trips", tags = {"Trip"})
     @GetMapping
-    public ResponseEntity<List<Trip>> getTrips() {
-        List<Trip> trips = tripService.getTrips();
+    public ResponseEntity<List<TripDTO>> getTrips() {
+        List<Trip> tripsRaw = tripService.getTrips();
+        List<TripDTO> trips = new ArrayList<>();
+        for (Trip trip : tripsRaw) {
+        	trips.add(new TripDTO(trip));
+        }
 
-        if (trips.size() != 0) return ResponseEntity.ok(trips);
+        if (!trips.isEmpty()) return ResponseEntity.ok(trips);
 
         return ResponseEntity.noContent().build();
     }
@@ -44,10 +58,10 @@ public class TripController {
      */
     @ApiOperation(value = "Return trip by ID", tags = {"Trip"})
     @GetMapping("/{id}")
-    public ResponseEntity<Trip> getTripById(@PathVariable("id") int id) {
+    public ResponseEntity<TripDTO> getTripById(@PathVariable("id") int id) {
         Optional<Trip> trip = tripService.getTripById(id);
 
-        if (trip.isPresent()) return ResponseEntity.ok(trip.get());
+        if (trip.isPresent()) return ResponseEntity.ok(new TripDTO(trip.get()));
 
         return ResponseEntity.noContent().build();
     }
@@ -60,10 +74,14 @@ public class TripController {
      */
     @ApiOperation(value = "Returns trips by driver ID", tags = {"Driver", "Trip"})
     @GetMapping("/driver/{driverId}")
-    public ResponseEntity<List<Trip>> getTripsByDriverId(@PathVariable("driverId") int driverId) {
-        List<Trip> trips = tripService.getTripsByDriverId(driverId);
+    public ResponseEntity<List<TripDTO>> getTripsByDriverId(@PathVariable("driverId") int driverId) {
+        List<Trip> tripsRaw = tripService.getTripsByDriverId(driverId);
+        List<TripDTO> trips = new ArrayList<>();
+        for (Trip trip : tripsRaw) {
+        	trips.add(new TripDTO(trip));
+        }
 
-        if (trips.size() != 0) return ResponseEntity.ok(trips);
+        if (!trips.isEmpty()) return ResponseEntity.ok(trips);
 
         return ResponseEntity.noContent().build();
     }
@@ -76,10 +94,14 @@ public class TripController {
      */
     @ApiOperation(value = "Returns trips by rider ID", tags = {"Rider", "Trip"})
     @GetMapping("/rider/{riderId}")
-    public ResponseEntity<List<Trip>> getTripsByRiderId(@PathVariable("riderId") int riderId) {
-        List<Trip> trips = tripService.getTripsByRiderId(riderId);
+    public ResponseEntity<List<TripDTO>> getTripsByRiderId(@PathVariable("riderId") int riderId) {
+        List<Trip> tripsRaw = tripService.getTripsByRiderId(riderId);
+        List<TripDTO> trips = new ArrayList<>();
+        for (Trip trip : tripsRaw) {
+        	trips.add(new TripDTO(trip));
+        }
 
-        if (trips.size() != 0) return ResponseEntity.ok(trips);
+        if (!trips.isEmpty()) return ResponseEntity.ok(trips);
 
         return ResponseEntity.noContent().build();
     }
@@ -91,12 +113,12 @@ public class TripController {
      */
     @ApiOperation(value = "Adds a new trip")
     @PostMapping
-    public ResponseEntity<Trip> addTrip(@Valid @RequestBody Trip trip) {
+    public ResponseEntity<TripDTO> addTrip(@Valid @RequestBody Trip trip) {
         Optional<Trip> newTrip = tripService.getTripById(trip.getTripId());
 
         if (newTrip.isPresent()) return ResponseEntity.badRequest().build();
 
-        return ResponseEntity.status(201).body(tripService.addTrip(trip));
+        return ResponseEntity.status(201).body(new TripDTO(tripService.addTrip(trip)));
     }
 
     /**
@@ -107,12 +129,73 @@ public class TripController {
      */
     @ApiOperation(value = "Updates a trip by ID", tags = {"Trip"})
     @PutMapping
-    public ResponseEntity<Trip> updateTrip(@Valid @RequestBody Trip trip) {
+    public ResponseEntity<TripDTO> updateTrip(@Valid @RequestBody Trip trip) {
         Optional<Trip> existingTrip = tripService.getTripById(trip.getTripId());
 
-        if (existingTrip.isPresent()) return ResponseEntity.status(201).body(tripService.updateTrip(trip));
+        if (existingTrip.isPresent()) return ResponseEntity.status(201).body(new TripDTO(tripService.updateTrip(trip)));
 
         return ResponseEntity.badRequest().build();
+    }
+    
+    /**
+     * HTTP POST method (/trips/{tripId}/rider)
+     * 
+     * @param tripId represents the id of the trip the person is joining
+     * @param rider represents the User joining the trip.
+     * @return The newly updated Trip object.
+     */
+    @ApiOperation(value = "Adds a user to a trip", tags = {"Rider", "Trip"})
+    @PostMapping("/{tripId}/rider")
+    public ResponseEntity<TripDTO> updateTripRider(@PathVariable("tripId") int tripId, @Valid @RequestBody UserDTO rider){
+    	Optional<Trip> existingTrip = tripService.getTripById(tripId);
+    	Optional<User> existingUser = userService.getUserById(rider.getUserId());
+    	
+    	if(existingTrip.isPresent() && existingUser.isPresent()) {
+    		Trip trip = existingTrip.get();
+    		List<User> riders = trip.getRiders();
+    		riders.add(existingUser.get());
+    		int numberOfSeats = trip.getAvailableSeats() - 1;
+    		if(numberOfSeats < 0)return ResponseEntity.status(HttpStatus.CONFLICT).build();
+    		trip.setAvailableSeats(numberOfSeats);
+    		tripService.updateTrip(trip);
+    		return ResponseEntity.accepted().body(new TripDTO(trip));
+    	}else if(!existingTrip.isPresent()) {
+    		return ResponseEntity.notFound().build();
+    	}else {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    	}
+    }
+    
+    /**
+     * HTTP DELETE method (/trips/{tripId}/rider)
+     * 
+     * @param tripId represents the id of the trip the person is joining
+     * @param rider represents the User joining the trip.
+     * @return The newly updated Trip object.
+     */
+    @ApiOperation(value = "Removes a user from a trip", tags = {"Rider", "Trip"})
+    @DeleteMapping("/{tripId}/rider")
+    public ResponseEntity<TripDTO> deleteTripRider(@PathVariable("tripId") int tripId, @Valid @RequestBody UserDTO rider){
+    	Optional<Trip> existingTrip = tripService.getTripById(tripId);
+    	Optional<User> existingUser = userService.getUserById(rider.getUserId());
+    	
+    	if(existingTrip.isPresent() && existingUser.isPresent()) {
+    		Trip trip = existingTrip.get();
+    		List<User> riders = trip.getRiders();
+    		int riderLength = riders.size();
+    		riders.remove(existingUser.get());
+    		if(riders.size() < riderLength) {
+				trip.setAvailableSeats(trip.getAvailableSeats() + 1);
+				tripService.updateTrip(trip);
+				return ResponseEntity.accepted().body(new TripDTO(trip));
+    		}else {
+				return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).build();
+    		}
+    	}else if(!existingTrip.isPresent()) {
+    		return ResponseEntity.notFound().build();
+    	}else {
+    		return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    	}
     }
 
     /**
