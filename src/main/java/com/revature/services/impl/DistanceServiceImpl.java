0a74ac6d6b645d912f1b02aea4876ba781406962
nyuan13 <yuan.nancy1@gmail.com>
2020-04-29 12:15:00 -0400

    added helper method, populated index map in computeClosest method

--- src/main/java/com/revature/services/impl/DistanceServiceImpl.java
+++ src/main/java/com/revature/services/impl/DistanceServiceImpl.java
@@ -1,5 +1,7 @@
 package com.revature.services.impl;
 
+import com.google.common.collect.ArrayListMultimap;
+import com.google.common.collect.Multimap;
 import com.google.maps.DistanceMatrixApi;
 import com.google.maps.DistanceMatrixApiRequest;
 import com.google.maps.GeoApiContext;
@@ -87,21 +89,59 @@ public class DistanceServiceImpl implements DistanceService {
 	 * @param top            The number of results you want
 	 * @return The set of pairs of indices of origins to destinations
 	 */
-	private Map<Integer, Integer> computeClosest(DistanceMatrix distanceMatrix, int top) {
-		Map<Integer, Integer> indexMap = new HashMap<>();
-		Map<Long, Map.Entry<Integer, Integer>> distanceMap = new HashMap<>();
-		List<Long> list = new LinkedList<>();
+	private int[][] computeClosest(DistanceMatrix distanceMatrix, int top) {
+		// Map of origin index and destination index
+		int[][] indexMap = new int[top][2];
+		// Multimap is a map that allows for duplicate entries
+		Multimap<Long, Map.Entry<Integer, Integer>> distanceMap = ArrayListMultimap.create();
+		LinkedList<Long> list = new LinkedList<>();
+		
 		for (int x = 0; x < distanceMatrix.rows.length; x++) {
 			for (int y = 0; y < distanceMatrix.rows[x].elements.length; y++) {
 				long distance = distanceMatrix.rows[x].elements[y].distance.inMeters;
-				//Store only the closest top values based on this number
-				for (int z = 0; z < top; z++) {
-					//Compare distance to entry in list and if shorter store in list
+				
+				// Store only the closest top values based on this number
+				// This list is always sorted
+				list.add(getIndex(list, distance, top), distance);
+				if (list.size() > top) {
+					list.pop();
 				}
-
+				
+				distanceMap.put(distance, new AbstractMap.SimpleEntry<>(x, y));
 			}
 		}
+		
+		// Adds closest indices to the indexMap
+		int i = 0;
+		while (i < list.size()) {
+			Collection<Map.Entry<Integer, Integer>> ties = distanceMap.get(list.get(i));
+			for (Map.Entry<Integer, Integer> tie : ties) {
+				indexMap[i][0] = tie.getKey();
+				indexMap[i][1] = tie.getValue();
+				i++;
+			}
+		}
+		
 		return indexMap;
+	}
+	
+	/**
+	 * Gets the index of the distance list to maintain ordering of the closest top distances.
+	 * Simply a helper method.
+	 * 
+	 * @param list
+	 * @param distance
+	 * @param top
+	 * @return the position to insert the new distance
+	 */
+	private int getIndex(List<Long> list, long distance, int top) {
+		for (int z = 0; z < top && z < list.size(); z++) {
+			//Compare distance to entry in list and if shorter store in list
+			if (distance < list.get(z)) {
+				return z;
+			}
+		}
+		return list.size();
 	}
 
 	public String getGoogleMAPKey() {
