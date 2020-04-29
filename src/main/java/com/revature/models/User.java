d70e4b522bf3cc289c6605fbd02ac1a340d8f51f
Jon Conner <jonathan.g.conner@gmail.com>
2020-04-29 15:53:33 -0400

    User Model - Organized imports and refactored to use @Data

--- src/main/java/com/revature/models/User.java
+++ src/main/java/com/revature/models/User.java
@@ -2,16 +2,8 @@ package com.revature.models;
 
 import java.io.Serializable;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.JoinColumn;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.Table;
+import javax.persistence.*;
+
 import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
@@ -22,16 +14,12 @@ import org.hibernate.annotations.LazyCollection;
 import org.hibernate.annotations.LazyCollectionOption;
 import org.springframework.stereotype.Component;
 
-import lombok.EqualsAndHashCode;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
-import lombok.ToString;
+import lombok.*;
 
 @Component
 @Entity
 @Table(name="users")
-@Getter @NoArgsConstructor @Setter @EqualsAndHashCode @ToString
+@Data @NoArgsConstructor
 public class User implements Serializable {
 	private static final long serialVersionUID = 1L;
 	@Id
