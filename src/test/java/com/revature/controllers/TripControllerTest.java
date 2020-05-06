c5243d59d4cca1dd5451558521c95a0b2487ab74
Jean Lucas <jean@4ray.co>
2020-05-05 23:50:05 -0400

    Get 100% test coverage for the Trip controller

--- src/test/java/com/revature/controllers/TripControllerTest.java
+++ src/test/java/com/revature/controllers/TripControllerTest.java
@@ -60,14 +60,34 @@ public class TripControllerTest {
     }
 
     @Test
+    public void testGetTripsFail() throws Exception {
+        List<TripDTO> trips = new ArrayList<>();
+
+        when(tripService.getTripsDTO()).thenReturn(trips);
+
+        mockMvc.perform(get("/trips"))
+                .andExpect(status().isNoContent());
+    }
+
+    @Test
     public void testGetTripById() throws Exception {
         Trip trip = MockObjects.getTrip();
 
         when(tripService.getTripById(1)).thenReturn(Optional.of(trip));
 
-        mockMvc.perform(get("/trips/{id}", 1))
+        mockMvc.perform(get("/trips/{id}", trip.getTripId()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.tripId").value(1));
+    }
+
+    @Test
+    public void testGetTripByIdFail() throws Exception {
+        Trip trip = MockObjects.getTrip();
+
+        when(tripService.getTripById(2)).thenReturn(Optional.of(trip));
+
+        mockMvc.perform(get("/trips/{id}", trip.getTripId()))
+                .andExpect(status().isNoContent());
     }
 
     @Test
@@ -101,6 +121,16 @@ public class TripControllerTest {
     }
 
     @Test
+    public void testUpdateTripFail() throws Exception {
+        Trip trip = MockObjects.getTrip();
+
+        mockMvc.perform(put("/trips")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(trip)))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
     public void testDeleteTripById() throws Exception {
         Trip trip = MockObjects.getTrip();
         String success = "Trip with id: " + trip.getTripId() + " was deleted";
@@ -108,9 +138,17 @@ public class TripControllerTest {
         when(tripService.getTripById(trip.getTripId())).thenReturn(Optional.of(trip));
         when(tripService.deleteTripById(1)).thenReturn(success);
 
-        mockMvc.perform(delete("/trips/{id}", 1))
+        mockMvc.perform(delete("/trips/{id}", trip.getTripId()))
                 .andExpect(status().isAccepted())
                 .andExpect(jsonPath("$").value(success));
+    }
+
+    @Test
+    public void testDeleteTripByIdFail() throws Exception {
+        Trip trip = MockObjects.getTrip();
+
+        mockMvc.perform(delete("/trips/{id}", trip.getTripId()))
+                .andExpect(status().isNotFound());
     }
 
     @Test
@@ -131,9 +169,26 @@ public class TripControllerTest {
 
         when(tripService.getTripsByRiderIdDTO(rider.getUserId())).thenReturn(trips);
 
-        mockMvc.perform(get(String.format("%s%s%s", "/trips/rider", "?riderId=", rider.getUserId())))
+        mockMvc.perform(get(String.format(
+                "%s%s%s",
+                "/trips/rider",
+                "?riderId=",
+                rider.getUserId()
+        )))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void testGetTripsByRiderIdFail() throws Exception {
+        UserDTO rider = new UserDTO(MockObjects.getAdonis());
+        mockMvc.perform(get(String.format(
+                "%s%s%s",
+                "/trips/rider",
+                "?riderId=",
+                rider.getUserId()
+        )))
+                .andExpect(status().isNoContent());
     }
 
     @Test
@@ -156,6 +211,21 @@ public class TripControllerTest {
         )))
                 .andExpect(status().isAccepted())
                 .andExpect(jsonPath("$.riders", hasSize(1)));
+    }
+
+    @Test
+    public void testUpdateTripRidersFail() throws Exception {
+        Trip trip = MockObjects.getTrip();
+        User rider = MockObjects.getAdonis();
+
+        mockMvc.perform(post(String.format(
+                "%s%s%s%s",
+                "/trips/rider?tripId=",
+                trip.getTripId(),
+                "&riderId=",
+                rider.getUserId()
+        )))
+                .andExpect(status().isNotFound());
     }
 
     @Test
@@ -183,6 +253,47 @@ public class TripControllerTest {
     }
 
     @Test
+    public void testDeleteTripRiderFailToRemove() throws Exception {
+        Trip trip = MockObjects.getTrip();
+        User rider1 = MockObjects.getAdonis();
+        rider1.setUserId(1);
+        User rider2 = MockObjects.getAdonis();
+        rider2.setUserId(2);
+        List<User> riders = new ArrayList<>();
+        riders.add(rider1);
+        trip.setRiders(riders);
+
+        when(tripService.getTripById(trip.getTripId())).thenReturn(Optional.of(trip));
+        when(userService.getUserById(rider2.getUserId())).thenReturn(Optional.of(rider2));
+
+        when(tripService.updateTrip(trip)).thenReturn(trip);
+
+        mockMvc.perform(delete(String.format(
+                "%s%s%s%s",
+                "/trips/rider?tripId=",
+                trip.getTripId(),
+                "&riderId=",
+                rider2.getUserId()
+        )))
+                .andExpect(status().isNotAcceptable());
+    }
+
+    @Test
+    public void testDeleteTripRiderFailNotFound() throws Exception {
+        Trip trip = MockObjects.getTrip();
+        User rider = MockObjects.getAdonis();
+
+        mockMvc.perform(delete(String.format(
+                "%s%s%s%s",
+                "/trips/rider?tripId=",
+                trip.getTripId(),
+                "&riderId=",
+                rider.getUserId()
+        )))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
     public void testGetTripsByDriverId() throws Exception {
         List<TripDTO> trips = new ArrayList<>();
         TripDTO trip1 = new TripDTO(MockObjects.getTrip());
@@ -201,5 +312,21 @@ public class TripControllerTest {
         )))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$", hasSize(2)));
+    }
+
+    @Test
+    public void testGetTripsByDriverIdFail() throws Exception {
+        UserDTO driver = new UserDTO(MockObjects.getAdonis());
+        UserDTO rider = new UserDTO(MockObjects.getAdonis());
+
+        mockMvc.perform(get(String.format(
+                "%s%s%s%s%s",
+                "/trips/driver",
+                "?driverId=",
+                driver.getUserId(),
+                "&riderId=",
+                rider.getUserId()
+        )))
+                .andExpect(status().isNoContent());
     }
 }
